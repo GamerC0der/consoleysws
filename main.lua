00719bd399c54b8d2c4807ff026305de979003db
GamerC0der <159653318+GamerC0der@users.noreply.github.com>
2025-12-31 20:34:22 -0800

    Fighter Sprite

--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
 function love.load()
     skyImage = love.graphics.newImage("sky.png")
+    skyImage:setFilter("nearest", "nearest")
     skyData = love.image.newImageData("sky.png")
     bottomRed, bottomGreen, bottomBlue = skyData:getPixel(0, skyImage:getHeight() - 1)
+    fighterImage = love.graphics.newImage("fighter.png")
 
     screenWidth = love.graphics.getWidth()
     screenHeight = love.graphics.getHeight()
@@ -9,6 +11,8 @@ function love.load()
     verticalOffset = -0.1 * skyImage:getHeight() * imageScale
 
     scrollPosition = 0
+    fighterX = screenWidth / 2
+    fighterY = screenHeight - fighterImage:getHeight() * 0.3
 end
 
 function love.resize(width, height)
@@ -23,6 +27,11 @@ function love.update(dt)
     if scrollPosition >= skyImage:getWidth() * imageScale then
         scrollPosition = scrollPosition - skyImage:getWidth() * imageScale
     end
+
+    if love.keyboard.isDown("left") then fighterX = fighterX - 200 * dt end
+    if love.keyboard.isDown("right") then fighterX = fighterX + 200 * dt end
+    if love.keyboard.isDown("up") then fighterY = fighterY - 200 * dt end
+    if love.keyboard.isDown("down") then fighterY = fighterY + 200 * dt end
 end
 
 function love.draw()
@@ -32,4 +41,6 @@ function love.draw()
     love.graphics.setColor(bottomRed, bottomGreen, bottomBlue)
     love.graphics.rectangle("fill", 0, screenHeight * 0.9, screenWidth, screenHeight * 0.1)
     love.graphics.setColor(1, 1, 1)
+
+    love.graphics.draw(fighterImage, fighterX - (fighterImage:getWidth() * 0.3) / 2, fighterY, 0, 0.3, 0.3)
 end
